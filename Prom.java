aaed6f6f930e819b075db67afcf8a35690605e48
Kaciade <valerypolyakov60@gmail.com>
2023-08-08 23:25:40 +0200

    Добавлено описание формата ввода данных

--- Prom.java
+++ Prom.java
@@ -6,7 +6,7 @@ public class Prom {
     public static void main(String[] args) {
         try {
             // Получение данных от пользователя
-            System.out.println("Введите данные: ");
+            System.out.println("Введите данные в формате\nФамилия Имя Отчество дд.мм.гггг Номер_телефона Пол( m(мужской) / f(женский) ): ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             scanner.close();
